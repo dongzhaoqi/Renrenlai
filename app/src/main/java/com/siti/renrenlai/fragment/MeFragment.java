45a835f5db6fb9e2a668fbe054ea3b5345d4d1c6
dong <dong18765203770@163.com>
2016-04-28 16:33:51 +0800

    2016-04-28

--- app/src/main/java/com/siti/renrenlai/fragment/MeFragment.java
+++ app/src/main/java/com/siti/renrenlai/fragment/MeFragment.java
@@ -18,76 +18,111 @@ import com.siti.renrenlai.activity.MyProfileActivity;
 import com.siti.renrenlai.util.SharedPreferencesUtil;
 import com.siti.renrenlai.view.FragmentBase;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 /**
  * Created by Dong on 3/22/2016.
  */
 public class MeFragment extends FragmentBase implements View.OnClickListener {
 
+    @Bind(R.id.layout_name) RelativeLayout layout_name;
+    @Bind(R.id.layout_favorite) LinearLayout layout_favorite;
+    @Bind(R.id.layout_enroll) LinearLayout layout_enroll;
+    @Bind(R.id.layout_launch) LinearLayout layout_launch;
+    @Bind(R.id.layout_message) RelativeLayout layout_message;
+    @Bind(R.id.layout_invite) RelativeLayout layout_invite;
+    @Bind(R.id.layout_feedback) RelativeLayout layout_feedback;
+    @Bind(R.id.layout_logout) RelativeLayout layout_logout;
     private View view;
-    private RelativeLayout layout_name;
-    private LinearLayout layout_favorite, layout_enroll, layout_launch;
-    private RelativeLayout layout_logout, layout_message, layout_feedback;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_me,container,false);
+        view = inflater.inflate(R.layout.fragment_me, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        initView();
+        initTopBarForOnlyTitle("我的");
     }
 
-    private void initView() {
-        initTopBarForOnlyTitle("我的");
-        layout_name = (RelativeLayout) findViewById(R.id.layout_name);
-        layout_favorite = (LinearLayout) findViewById(R.id.layout_favorite);
-        layout_enroll = (LinearLayout) findViewById(R.id.layout_enroll);
-        layout_launch = (LinearLayout) findViewById(R.id.layout_launch);
 
-        layout_logout = (RelativeLayout) findViewById(R.id.layout_logout);
-        layout_message = (RelativeLayout) findViewById(R.id.layout_message);
-        layout_feedback = (RelativeLayout) findViewById(R.id.layout_feedback);
-
-        layout_name.setOnClickListener(this);
-        layout_favorite.setOnClickListener(this);
-        layout_enroll.setOnClickListener(this);
-        layout_launch.setOnClickListener(this);
-
-        layout_logout.setOnClickListener(this);
-        layout_message.setOnClickListener(this);
-        layout_feedback.setOnClickListener(this);
+    private void showShare() {
+        ShareSDK.initSDK(getActivity());
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.ssdk_oks_share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("仁人来");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        //oks.setImageUrl("http://img05.tooopen.com/images/20160108/tooopen_sy_153700436869.jpg");
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+        // 启动分享GUI
+        oks.show(getActivity());
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ShareSDK.stopSDK(getActivity());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.layout_name, R.id.layout_favorite, R.id.layout_enroll, R.id.layout_launch,
+            R.id.layout_message, R.id.layout_invite, R.id.layout_feedback, R.id.layout_logout})
     public void onClick(View v) {
         int id = v.getId();
         Intent intent = null;
-        switch (id){
+        switch (id) {
             case R.id.layout_name:
                 startAnimActivity(MyProfileActivity.class);
                 break;
             case R.id.layout_favorite:
-                intent = new Intent(getActivity(),MyActivity.class);
-                intent.putExtra("pos",0);
+                intent = new Intent(getActivity(), MyActivity.class);
+                intent.putExtra("pos", 0);
                 startAnimActivity(intent);
                 break;
             case R.id.layout_enroll:
-                intent = new Intent(getActivity(),MyActivity.class);
-                intent.putExtra("pos",1);
+                intent = new Intent(getActivity(), MyActivity.class);
+                intent.putExtra("pos", 1);
                 startAnimActivity(intent);
                 break;
             case R.id.layout_launch:
-                intent = new Intent(getActivity(),MyActivity.class);
-                intent.putExtra("pos",2);
+                intent = new Intent(getActivity(), MyActivity.class);
+                intent.putExtra("pos", 2);
                 startAnimActivity(intent);
                 break;
             case R.id.layout_message:
                 startAnimActivity(MessageActivity.class);
+                break;
+            case R.id.layout_invite:
+                showShare();
                 break;
             case R.id.layout_logout:
                 SharedPreferencesUtil.writeString(SharedPreferencesUtil
